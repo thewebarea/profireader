ed9cb6265d79195947146773d762e8bb065342f4
spaun299 <spaun1002@gmail.com>
2015-07-08 19:27:35 +0300

    new branch with application and database

---
+++ profapp/models/company.py
@@ -0,0 +1,16 @@
+from db_connect import metadata
+from sqlalchemy import Table, Column, Integer, String,Boolean
+from sqlalchemy.orm import mapper
+
+company_table=Table('company',metadata,
+                    Column('id',Integer,primary_key=True),
+                    Column('name',String(60)),
+                    Column('portal_consist',Boolean)
+                    )
+class Company(object):
+
+    def __init__(self,name,portal_consist=False):
+        self.name=name
+        self.portal_consist=portal_consist
+mapper(Company,company_table)
+
