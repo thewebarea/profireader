fcac89ce283ec0834767d699660741d49db862a4
spaun299 <spaun1002@gmail.com>
2015-07-16 17:43:39 +0300

    modify some filemanager files

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from profapp import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()  #app.run(debug=True)
+    app.run(host='127.12.3.4',port=8080)  #app.run(debug=True)
