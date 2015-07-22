fc4132e37420972114c13fa014334ad0f20514ec
spaun299 <spaun1002@gmail.com>
2015-07-22 13:36:49 +0300

    listing and rename are done

--- profapp/controllers/ctrl_filemanager.py
+++ profapp/controllers/ctrl_filemanager.py
@@ -3,29 +3,34 @@ from time import gmtime, strftime
 from stat import *
 from flask import jsonify, request, Blueprint
 
-files = os.listdir('/home/viktor/Downloads')
+root='/home/viktor/Downloads'
 static_bp = Blueprint('static', __name__)
 
 @static_bp.route('/filemanager/bridges/python/ctrl_filemanager.py', methods=['GET', 'POST'])
 def ctrl_filemanager():
+
     for params in request.json.values():
         if params['mode'] == 'list':
             print(listing())
             return jsonify(listing())
+        elif params['mode'] == 'rename':
 
+            print(params['path'], params['newPath'])
+            return jsonify(rename(params['path'], params['newPath']))
 
 
 def listing():
 
     info = []
+    files = os.listdir(root)
     for file in files:
-        st = os.stat('/home/viktor/Downloads/'+file)
+        st = os.stat(root+'/'+file)
         params = dict()
         params['size'] = st[ST_SIZE]
         params['date'] = strftime("%Y-%d-%m %X", gmtime(st[ST_MTIME]))
         params['name'] = os.path.basename(file)
         params['rights'] = 'drwxr-xr-x'
-        if os.path.isfile('/home/viktor/Downloads/'+file):
+        if os.path.isfile(root+'/'+file):
             params['type'] = 'file'
         else :
             params['type'] = 'dir'
@@ -33,4 +38,8 @@ def listing():
     file_list = {"result": info}
     return file_list
 
+def rename(path, new_path):
 
+    os.renames(root+path, root+new_path)
+    result = {"result": {"success": True, "error": None}}
+    return result
