7a927ce7ca05955f7e8d64198f3a42c0b6fedfb4
Oles Zaburannyi <kakabomba@gmail.com>
2015-08-17 20:16:08 +0300

    m

--- profapp/controllers/blueprints.py
+++ profapp/controllers/blueprints.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 general_bp = Blueprint('general', __name__)
 auth_bp = Blueprint('auth', __name__)
 user_bp = Blueprint('user', __name__)
-article_bp = Blueprint('articles', __name__)
+article_bp = Blueprint('article', __name__)
 filemanager_bp = Blueprint('filemanager', __name__)
 static_bp = Blueprint('static', __name__, static_url_path='')
 image_editor_bp = Blueprint('image_editor', __name__)
