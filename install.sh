e55eda63b07473fe260e7baf33103b9b606be5af
Oles Zaburannyi <kakabomba@gmail.com>
2015-08-23 17:50:07 +0300

    m

--- install.sh
+++ install.sh
@@ -51,17 +51,32 @@ function conf {
 }
 
 conf_comm() {
-    echo "Command we're going to execute:"
-    rd=`tput setaf 1`
+rd=`tput setaf 1`
     rst=`tput sgr0`
-    echo "${rd}"
-    echo "$1" | sed -e 's/"/\"/g' -e 's/^/    sudo bash -c "/g' -e 's/$/";/g'
+
+    if [[ "$2" == "sudo" ]]; then
+	echo "${rd}"
+        echo "Command we're going to execute (with sudo):"
+    else
+	echo "Command we're going to execute:"
+    fi
+    e
+    echo "$1" | sed -e 's/^/    /g'
     echo "${rst}"
     if conf; then
 	e
-        eval `echo "$1" | sed -e 's/"/\"/g' -e 's/^/sudo bash -c "/g' -e 's/$/";/g'`
-	if [[ "$2" == "" ]]; then
+	echo "$1" > /tmp/menu_command_run_confirmed.sh
+	if [[ "$2" == "sudo" ]]; then
+	    sudo bash /tmp/menu_command_run_confirmed.sh
+	else
+	    bash /tmp/menu_command_run_confirmed.sh
+	fi
+#        eval `echo "$1" | sed -e 's/"/\"/g' -e 's/^/sudo bash -c "/g' -e 's/$/";/g'`
+	if [[ "$4" == "" ]]; then
 	    ret
+	fi
+	if [[ "$3" != "" ]]; then
+	    next="$3"
 	fi
         true
     else
@@ -71,54 +86,19 @@ conf_comm() {
 
 
 function runsql {
-    conf_comm "su postgres -c \"echo \\\"$1\\\" | psql\""
+    conf_comm "su postgres -c \"echo \\\"$1\\\" | psql\"" sudo "$2"
     }
 
 function runsql_dump {
     profidb=`cat secret_data.py | grep 'DB_NAME' | sed -e 's/^\s*DB_NAME\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
     filenam=$(rr "$1" "$2")
-    conf_comm "su postgres -c 'cat $filenam | psql $profidb'"
+    conf_comm "su postgres -c 'cat $filenam | psql $profidb'" sudo "$3"
     }
 
 function menu_deb {
     conf_comm "apt-get update
-apt-get install libpq-dev libapache2-mod-wsgi"
+apt-get install libpq-dev libapache2-mod-wsgi" sudo hosts
     }
-
-function menu_db_backup {
-    profidb=`cat secret_data.py | grep 'DB_NAME' | sed -e 's/^\s*DB_NAME\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
-    conf_comm "su postgres -c 'echo \"ALTER DATABASE $profidb RENAME TO bak_$profidb""_$date\" | psql'"
-    }
-
-function menu_db_struct {
-    runsql_dump 'Enter sql structure filename' database_structure.sql
-    }
-
-function menu_db_initial {
-    runsql_dump 'Enter sql initial data filename' database_initial_data.sql
-    }
-
-function menu_db_create {
-    profidb=`cat secret_data.py | grep 'DB_NAME' | sed -e 's/^\s*DB_NAME\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
-    psqldb=$(rr 'Enter postgresql database name' $profidb)
-    
-    profiuser=`cat secret_data.py | grep 'DB_USER' | sed -e 's/^\s*DB_USER\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
-    runsql "CREATE DATABASE $psqldb WITH ENCODING 'UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8'  OWNER = $profiuser TEMPLATE=template0"
-    }
-
-function menu_db_user_pass {
-    echo "Going to create user/pass from secret data and create such user/pass using postgres user"
-    echo "If user exists, only password will be changed"
-    
-    profiuser=`cat secret_data.py | grep 'DB_USER' | sed -e 's/^\s*DB_USER\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
-    psqluser=$(rr 'Enter postgresql user' $profiuser)
-    
-    profipass=`cat secret_data.py | grep 'DB_PASSWORD' | sed -e 's/^\s*DB_PASSWORD\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
-    psqlpass=$(rr 'Enter postgresql password' $profipass)
-    runsql "CREATE USER $psqluser with password '$psqlpass'"
-    
-    }
-
 
 function menu_hosts {
     conf_comm "sed -i '/\(db\|web\|mail\).profi/d' /etc/hosts
@@ -126,7 +106,7 @@ sed -i '/\(companyportal\|aprofi\).d.ntaxa.com/d' /etc/hosts
 echo '' >> /etc/hosts
 echo '127.0.0.1 aprofi.d.ntaxa.com companyportal.d.ntaxa.com' >> /etc/hosts
 echo '127.0.0.1 db.profi web.profi mail.profi' >> /etc/hosts
-cat /etc/hosts"
+cat /etc/hosts" sudo secret_data
     }
 
 function menu_secret_data {
@@ -134,8 +114,8 @@ function menu_secret_data {
     echo "  wget --user='ntaxauser' --password='ntaxapassword' -O secret_data.txt http://x.d.ntaxa.com/profireader/secret_data.txt"
     echo "  mv secret_data.txt secret_data.py"
     echo "If exists, secret_data.py will be copied to secret_data.bak.py"
-    ntaxauser=$(rr 'Enter ntaxa user')
-    ntaxapass=$(rr "Enter ntaxa password")
+    ntaxauser=$(rr 'Enter ntaxa user:')
+    ntaxapass=$(rr "Enter ntaxa password:")
     conf_comm "wget --user='$ntaxauser' --password='$ntaxapass' http://x.d.ntaxa.com/profireader/secret_data.txt
 if [[ \"\$?\" == \"0\" ]]; then
     if [[ -f secret_data.py  ]]; then
@@ -145,7 +125,7 @@ if [[ \"\$?\" == \"0\" ]]; then
     mv secret_data.txt secret_data.py
 else
   echo 'wget failed!'
-fi"
+fi" nosudo  python_3
     }
 
 function menu_python_3 {
@@ -166,32 +146,72 @@ make install
 ln -s $destdir/bin/python3 /usr/bin/python3
 ln -s $destdir/bin/pyvenv /usr/bin/pyvenv
 cd /tmp
-rm -rf 'Python-$pversion'"
+rm -rf 'Python-$pversion'" sudo venv
     fi
     }
-
-function menu_port {
-    toport=$(rr 'redirect port 80 to port' 8080)
-    conf_comm "iptables -t nat -A OUTPUT  -d 127.0.0.1  -p tcp --dport 80 -j REDIRECT --to-port $toport"
-    }
-
-function menu_modules {
-    req=$(rr 'file with modules' requirements.txt)
-    destdir=$(rr 'venv directory' .venv)
-    conf_comm "source $destdir/bin/activate
-pip3 install -r $req"
-    }
-
-$destdir
 
 function menu_venv {
     destdir=$(rr 'destination dir for virtual directory' .venv)
     if [[ -e $destdir ]]; then
 	echo "error: $destdir exists"
     else
-	conf_comm "pyvenv $destdir"
+	conf_comm "pyvenv $destdir" nosudo modules
     fi
     }
+
+function menu_modules {
+    req=$(rr 'file with modules' requirements.txt)
+    destdir=$(rr 'venv directory' .venv)
+    conf_comm "source $destdir/bin/activate
+pip3 install -r $req" nosudo port
+    }
+
+function menu_port {
+    toport=$(rr 'redirect port 80 to port' 8080)
+    conf_comm "iptables -t nat -A OUTPUT  -d 127.0.0.1  -p tcp --dport 80 -j REDIRECT --to-port $toport" sudo db_user_bass
+    }
+
+function menu_db_user_pass {
+    echo "Going to create user/pass from secret data and create such user/pass using postgres user"
+    echo "If user exists, only password will be changed"
+    
+    profiuser=`cat secret_data.py | grep 'DB_USER' | sed -e 's/^\s*DB_USER\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
+    psqluser=$(rr 'Enter postgresql user' $profiuser)
+    
+    profipass=`cat secret_data.py | grep 'DB_PASSWORD' | sed -e 's/^\s*DB_PASSWORD\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
+    psqlpass=$(rr 'Enter postgresql password' $profipass)
+    runsql "CREATE USER $psqluser with password '$psqlpass'" db_create
+    
+    }
+
+function menu_db_create {
+    profidb=`cat secret_data.py | grep 'DB_NAME' | sed -e 's/^\s*DB_NAME\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
+    psqldb=$(rr 'Enter postgresql database name' $profidb)
+    
+    profiuser=`cat secret_data.py | grep 'DB_USER' | sed -e 's/^\s*DB_USER\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
+    runsql "CREATE DATABASE $psqldb WITH ENCODING 'UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8'  OWNER = $profiuser TEMPLATE=template0" db_struct
+    }
+
+function menu_db_struct {
+    runsql_dump 'Enter sql structure filename' database_structure.sql db_initial
+    }
+
+function menu_db_initial {
+    runsql_dump 'Enter sql initial data filename' database_initial_data.sql 'db_backup'
+    }
+
+function menu_db_backup {
+    profidb=`cat secret_data.py | grep 'DB_NAME' | sed -e 's/^\s*DB_NAME\s*=\s*['"'"'"]\([^'"'"'"]*\).*$/\1/g' `
+    runsql "ALTER DATABASE $profidb RENAME TO bak_$profidb""_$date" 'exit'
+    }
+
+
+
+
+
+
+
+
 
 next='_'
 
@@ -204,7 +224,7 @@ next='_'
 
 while :
 do
-next='exit'
+#next='exit'
 dialog --title "profireader" --nocancel --default-item $next --menu "Choose an option" 20 78 16 \
 "deb" "install deb packages" \
 "hosts" "create virtual domain zone in /etc/hosts" \
