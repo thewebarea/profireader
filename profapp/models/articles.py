15e60a03bd8a1a90de037cf461026fee1dd9f4a2
Oles Zaburannyi <kakabomba@gmail.com>
2015-08-27 12:06:37 +0300

    m

--- profapp/models/articles.py
+++ profapp/models/articles.py
@@ -46,22 +46,8 @@ class ArticleCompany(Base, PRBase):
     article = relationship('Article')
 
 
-
-    # def __init__(self, created_from_version_id, author_user_id=None, company_id=None, name='', short='', long=''):
-    #     self.author_user_id = author_user_id if author_user_id is not None else g.user_dict['id']
-    #
-    #     self.title = title
-    #     self.short = short
-    #     self.long = long
-    #     self.article = Article() if company_id is None else _A().filter(Article.id == ArticleCompany.get(created_from_version_id).article_id).one()
-
-
     def clone_for_company(self, company_id):
-        self.detach()
-        self.company_id = company_id
-        self.created_from_version_id = self.id
-        return self
-
+        return self.detach().attr({'company_id': company_id, 'created_from_version_id': self.id})
 
 class Article(Base, PRBase):
     __tablename__ = 'article'
@@ -84,23 +70,7 @@ class Article(Base, PRBase):
 
     @staticmethod
     def user_articles(user_id=None, before_id=None):
-
-
-
-# SELECT DISTINCT ON (article_version.company_id) article_version.*, per_group.count FROM article_version LEFT JOIN (SELECT company_id, count(id) as count FROM article_version
-# GROUP BY article_version.company_id) as per_company  ON
-# (per_company.company_id = article_version.company_id OR (per_group.company_id IS NULL AND article_version.company_id IS NULL))
-# ORDER BY company_id, id
-
-
         return _A().filter_by(author_user_id=user_id).all()
 
-    @staticmethod
-    def get_last_company_versions_for_user(article_id, author_user_id):
-        # grouped_by_company = aliased(ArticleVersion)
-        # grouped_by_company_subquery = db_session.query(ArticleVersion.company_id).filter_by(article_id=article_id, author_user_id=author_user_id).group_by(ArticleVersion.company_id).subquery()
-        # ret = db_session.query(ArticleVersion).distinct('article_version.company_id').filter_by(article_id=article_id, author_user_id=author_user_id).order_by(ArticleVersion.company_id, desc(ArticleVersion.id)).outerjoin(grouped_by_company_subquery, grouped_by_company_subquery.c.company_id == ArticleVersion.company_id)
-        # ret = ret.all()
-        return []
 
 
