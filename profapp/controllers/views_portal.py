9d46c39b87c5ab0e64b554a5e4b134f34915402f
spaun299 <spaun1002@gmail.com>
2015-08-28 15:11:04 +0300

    update portal

--- profapp/controllers/views_portal.py
+++ profapp/controllers/views_portal.py
@@ -1,5 +1,6 @@
 from .blueprints import portal_bp
 from flask import render_template, request, url_for, g, redirect
+from flask.ext.login import login_required
 from ..models.company import Company, UserCompanyRight, Right
 # from phonenumbers import NumberParseException
 from .errors import SubscribeToOwn
@@ -15,11 +16,15 @@ def apply_company():
     return redirect(url_for('portal.partners', company_id=data['comp_id']))
 
 @portal_bp.route('/partners/<string:company_id>/')
+@login_required
 def partners(company_id):
 
     comp = Company().query_company(company_id=company_id)
-    companies_partners = CompanyPortal.show_companies_on_portal(company_id)
+    companies_partners = CompanyPortal().show_companies_on_my_portal(company_id)
+    portals_partners = CompanyPortal().show_my_portals(company_id)
+
     return render_template('company_partners.html',
                            comp=comp,
-                           companies_partners=companies_partners
+                           companies_partners=companies_partners,
+                           portals_partners=portals_partners
                            )
--- profapp/models/portal.py
+++ profapp/models/portal.py
@@ -19,6 +19,19 @@ class Portal(Base):
         self.portal_plan_id = portal_plan_id
         self.company_owner_id = company_owner_id
 
+    @staticmethod
+    def own_portal(company_id):
+        try:
+            ret = db(Portal, company_owner_id=company_id).one()
+            return ret
+        except:
+            pass
+
+    @staticmethod
+    def query_portal(portal_id):
+        ret = db(Portal, id=portal_id).one()
+        return ret
+
 class PortalPlan(Base):
 
     __tablename__ = 'portal_plan'
@@ -34,20 +47,44 @@ class CompanyPortal(Base):
     id = Column(TABLE_TYPES['id_profireader'], nullable=False, primary_key=True)
     company_id = Column(TABLE_TYPES['id_profireader'], ForeignKey('company.id'))
     portal_id = Column(TABLE_TYPES['id_profireader'], ForeignKey('portal.id'))
+    company_portal_plan_id = Column(TABLE_TYPES['id_profireader'])
 
-    def __init__(self, company_id=None, portal_id=None):
+    def __init__(self, company_id=None, portal_id=None, company_portal_plan_id=None):
         self.company_id = company_id
         self.portal_id = portal_id
+        self.company_portal_plan_id = company_portal_plan_id
+
+    @staticmethod
+    def all_companies_on_portal(portal_id):
+        comp_port = db(CompanyPortal, portal_id=portal_id).all()
+        if not comp_port:
+            return ['Your portal does not have any companies partners']
+        comp = []
+        for company in comp_port:
+            comp.append(db(Company, id=company.company_id).one())
+        return comp
 
     @staticmethod
     def apply_company_to_portal(company_id, portal_id):
-        db_session.add(CompanyPortal(company_id=company_id, portal_id=portal_id))
+        db_session.add(CompanyPortal(company_id=company_id, portal_id=portal_id,
+                                     company_portal_plan_id=Portal().query_portal(portal_id).portal_plan_id))
         db_session.commit()
 
     @staticmethod
-    def show_companies_on_portal(company_id):
+    def show_companies_on_my_portal(company_id):
+
+        portal = Portal().own_portal(company_id)
+        if portal:
+            return CompanyPortal().all_companies_on_portal(portal.id)
+        else:
+            return False
+
+    @staticmethod
+    def show_my_portals(company_id):
+        comp_port = db(CompanyPortal, company_id=company_id).all()
+        if not comp_port:
+            return ['Your company does not subscribed to any portal']
         comp = []
-        portal = db(Portal, company_owner_id=company_id).one()
-        for company in db(CompanyPortal, portal_id=portal.id).all():
-            comp.append(db(Company, id=company.company_id).one())
+        for portal in comp_port:
+            comp.append(Portal().query_portal(portal.portal_id))
         return comp
