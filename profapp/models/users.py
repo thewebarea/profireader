6847ca9d55251df850048df8b815aedab584d9dd
bubanoid <an.andrusyk@gmail.com>
2015-09-17 15:11:46 +0300

    improve user_rights_in_company function in User class

--- profapp/models/users.py
+++ profapp/models/users.py
@@ -461,12 +461,8 @@ class User(Base, UserMixin, PRBase):
     #def is_administrator(self):
     #    return self.can(Permission.ADMINISTER)
     def user_rights_in_company(self, company_id):
-
-        user_rights = []
-        if self.employer_assoc.filter_by(company_id=company_id).count():
-            user_rights_int = self.employer_assoc.filter_by(company_id=company_id).one().rights
-            user_rights = list(Right.transform_rights_into_set(user_rights_int))
-        return user_rights
+        user_company = self.employer_assoc.filter_by(company_id=company_id).first()
+        return list(Right.transform_rights_into_set(user_company.rights)) if user_company else []
 
 class Group(Base, PRBase):
 
