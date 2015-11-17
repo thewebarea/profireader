ed27bfbe0b5de6079760733a91908168d5d1a260
spaun299 <spaun1002@gmail.com>
2015-11-17 16:27:45 +0200

    m

--- profapp/controllers/views_company.py
+++ profapp/controllers/views_company.py
@@ -77,24 +77,27 @@ def materials_load(json, company_id):
 
     page = json.get('search')['page'] if json.get('search') else 1
     search_text = json.get('search_text')
-    # params = {'search_text': json.get('search_text'), 'company_id': company_id}
+    # print(json)
     # article_status = json.get('chosen_status')
     # original_chosen_status = None
     # original_chosen_status
-
-    # if chosen_portal_id:
-    #     params['portal_id'] = chosen_portal_id
+    params = {}
+    if json.get('portal_id'):
+        params['portal_id'] = json.get('portal_id')
     # if article_status:
     #     params['status'] = article_status
-    subquery = ArticleCompany.subquery_company_articles(search_text=search_text, company_id=company_id,
-                                                        **json.get('filter'))
+    # print(json.get('filter'))
+    # print(company_id)
+    subquery = ArticleCompany.subquery_company_articles(search_text=search_text,
+                                                        company_id=company_id,
+                                                        **params)
     articles, pages, current_page = pagination(subquery, page=page, items_per_page=5)
     portals = ArticlePortalDivision.get_portals_where_company_send_article(company_id)
 
     statuses = {status: status for status in ARTICLE_STATUS_IN_PORTAL.all}
     statuses['All'] = 'All'
 
-    return {'articles': [{'article': a.get_client_side_dict(),
+    return {'materials': [{'article': a.get_client_side_dict(),
                           'portals_count': len(a.get_client_side_dict()['portal_article']) + 1}
                          for a in articles],
             'portals': portals,
